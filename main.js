a00445870eca21407082857979629c4ad9810668
Bhargavi <B.patel20@share.epsb.ca>
2023-04-12 08:56:25 -0600

    fyjry

--- main.js
+++ main.js
@@ -36,6 +36,4 @@ function btnClicked() {
   } else {
     document.getElementById("answer4-in").style.borderColor = "red ";
   }
-
-  let results = Math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2);
 }
