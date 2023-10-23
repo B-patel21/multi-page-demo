1113713cbfddca7388656aef7d0ef739060a29f0
B-patel21 <bhargavipatel2007@gmail.com>
2023-10-23 13:16:48 -0600

    yjnfcgtsfgtnhj

--- main.js
+++ main.js
@@ -3,64 +3,42 @@ document.getElementById("check-btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Inputs
-  let name = document.getElementById("name-in").value;
-  console.log(name);
-  let answer1 = document.getElementById("answer1-in").value.toLowerCase();
-  console.log(answer1);
-  let answer2 = document.getElementById("answer2-in").value.toLowerCase();
-  console.log(answer2);
-  let answer3 = document.getElementById("answer3-in").value.toLowerCase();
-  console.log(answer3);
-  let answer4 = document.getElementById("answer4-in").value.toLowerCase();
-  console.log(answer4);
+  
   let correct = 0;
   // Check Answer
 
   let score = 0;
-  score += markQuestion(answer1, "12");
-  score += markQuestion(answer2, "bamboo");
-  score += markQuestion(answer3, "pink");
-  score += markQuestion(answer4, "south-west china");
+  score += markQuestion(1, "12");
+  score += markQuestion(2, "bamboo");
+  score += markQuestion(3, "pink");
+  score += markQuestion(4, "south-west china");
 
   document.getElementById("out-score").innerHTML = correct;
-  document.getElementById("percent-score").innerHTML = (correct / 4) * 100;
+  document.getElementById("percent-score").innerHTML = (score / 4) * 100;
 
   if (correct <= 2) {
-    document.getElementById("output");
+    document.getElementById("output").innerHTML = "Try again";
   } 
-  if (correct > 2 && correct < 3) {
-    document.getElementById("output");
+  if (correct ===3) {
+    document.getElementById("output").innerHTML = "Good Job";
   }
   if (correct === 4) {
-    document.getElementById("output");
+    document.getElementById("output").innerHTML = "Awesome";
   }
 }
 
-<<<<<<< HEAD
-// function markQuestion(qNum, correctAnswer) {
-//   let userElement = document.getElementById("answer" + ${qNum}).value; 
-//   let feedbackEl = document.getElementById()
-//   let marks = 0
-//   if (userElement === correctAnswer){
-//     return marks = 1
-//     document.getElementById()
-//   }
-// }
-=======
 function markQuestion(qNum, correctAnswer) {
-  let userAnswer = document.getElementById("answer" + ${qNum}).value.toLowerCase; 
-  let feedbackEl = document.getElementById("feedback").value;
-  let mark = 0;
+  let inputEl = document.getElementById(`answer${qNum}`); 
+  let feedbackEl = document.getElementById(`feedback${qNum}`);
+  let useranswer = inputEl.value.toLowerCase();
 
-  if (userAnswer === correctAnswer) {
-    feedbackEl.innerHTML = "Correct!";
-    feedbackEl.style.color = "green";
-    mark = 1;
+  if (useranswer === correctAnswer){
+    inputEl.style.border = "2px solid green";
+    feedbackEl.innerHTML = "Correct";
+    return 1 ; 
   } else {
-    feedbackEl.innerHTML = "Incorrect. Correct answer is " + correctAnswer;
-    feedbackEl.style.color = "red";
+    inputEl.style.border = "2px solid red";
+    feedbackEl.innerHTML= "Incorrect";
+    return 0;
   }
-
-  return mark;
-}
->>>>>>> 89f69ef2fddb73279e704f7a300752531aefb9af
+}      
